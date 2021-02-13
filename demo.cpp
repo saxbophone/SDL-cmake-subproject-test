82fe3bba1daa8fc70e248aea34cd4d4162928b1e
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-02-13 06:44:52 +0000

    Add basic tiny debug program that tests we're linked with SDL Just prints out SDL version and platform we're on

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,16 @@
+#include <cstdio>
+
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
 int main(void) {
+    if (SDL_Init(0) < 0) {
+        fprintf(stderr, "SDL_Init() failed with: %s", SDL_GetError());
+        return -1;
+    }
+    SDL_version version;
+    SDL_GetVersion(&version);
+    printf("SDL version %d.%d.%d on %s\n", version.major, version.minor, version.patch, SDL_GetPlatform());
+    SDL_Quit();
     return 0;
 }
